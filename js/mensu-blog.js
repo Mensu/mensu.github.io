941afe39782baf89fe25cfa610ffe41185746543
Mensu <yxshw55@qq.com>
2016-03-01 01:19:35 +0800

    translate; fix layout
    translate Duoshuo comment box to English; fix layout problems on
    comment box

--- js/mensu-blog.js
+++ js/mensu-blog.js
@@ -1,6 +1,6 @@
 /* like button */
 
-// keep "like" button on the same line as "share to"
+// keep "like" button on the same line as "share on"
 var previousDistance_dsShare_dsThread = "0px", class_dsMeta_previousWidth = 0;
 var dsElementsReady = false;
 var class_pseudoDsMeta, class_dsShare, class_dsThread, class_dsMeta;
@@ -13,7 +13,7 @@ setInterval(function () {
     class_dsMeta = document.getElementsByClassName("ds-meta");
     if (typeof(class_dsMeta[0]) == "undefined" || typeof(class_pseudoDsMeta[0]) == "undefined") return;
     else dsElementsReady = true;
-    modifyShareTo();
+    modifyShareOn();
   }
 
   // raise "like" button
@@ -22,7 +22,7 @@ setInterval(function () {
     previousDistance_dsShare_dsThread = class_dsMeta[0].style.top = currentDistance_dsShare_dsThread;
   }
 
-  // change the offset of "share to" as the width of "like" button changes
+  // change the offset of "share on" as the width of "like" button changes
   var class_dsMeta_currentWidth = class_dsMeta[0].clientWidth;
   if (class_dsMeta_previousWidth != class_dsMeta_currentWidth) {
     class_pseudoDsMeta[0].style.width = (class_dsMeta_currentWidth > 0) ? (class_dsMeta_currentWidth + 20 + "px") : "0";
@@ -32,30 +32,16 @@ setInterval(function () {
 }, 60);
 
 
-/* share to */
+/* share on */
 
-function modifyShareTo() {
-  // make sure to get the "share to" element
+function modifyShareOn() {
+  // make sure to get the "share on" element
   var class_dsShareIcons = undefined;
   while (typeof(class_dsShareIcons) == "undefined") class_dsShareIcons = document.getElementsByClassName("ds-share-icons-16");
 
-  // fix "undefined" problem, translate the names of the sites to English
-  document.getElementsByClassName("ds-weibo")[1].innerHTML = "Sina Weibo";
-  document.getElementsByClassName("ds-qzone")[0].innerHTML = "Qzone";
-  document.getElementsByClassName("ds-qqt")[0].innerHTML = "Tencent Weibo";
+  // fix "undefined" problem
   document.getElementsByClassName("ds-douban")[0].innerHTML = "Douban";
-  document.getElementsByClassName("ds-google")[0].innerHTML = "Google+";
   document.getElementsByClassName("ds-youdao")[0].innerHTML = "Youdao CloudNote";
-  document.getElementsByClassName("ds-weibo flat")[0].innerHTML = "Weibo";
-  document.getElementsByClassName("ds-more")[0].innerHTML = "Share to";
-  document.getElementsByClassName("ds-thread-cancel-like")[0].innerHTML = "Cancel like";
-
-  // remove sites that are not famous from "share to"
-  var sitesToBeRemoved = new Array("ds-sohu", "ds-renren", "ds-netease", "ds-kaixin", "ds-meilishuo", "ds-mogujie", "ds-baidu", "ds-taobao", "ds-wechat", "ds-diandian", "ds-huaban", "ds-duitang", "ds-pengyou", "ds-msn");
-  for (var i = 0; i < sitesToBeRemoved.length; i++) (function(name) {
-    var toBeRemoved = document.getElementsByClassName(name)[0].parentNode;
-  toBeRemoved.parentNode.removeChild(toBeRemoved);
-  }(sitesToBeRemoved[i]));
 }
 
 
